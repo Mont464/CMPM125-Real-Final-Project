fc453f54b633efff283393dd206acfe2318372e3
lweislak <150990328+lweislak@users.noreply.github.com>
2024-11-12 22:48:10 -0800

    Basic Player Controls
    Added movement for left, right, crouch, and jump

--- CMPM 125 Final with URP/Assets/Scripts/PlayerMovement.cs
+++ CMPM 125 Final with URP/Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,13 @@ using System.Diagnostics;
 using UnityEngine;
 
 //Crouch code found at: https://www.youtube.com/watch?v=xCxSjgYTw9c
-//isGrounded code found at: https://www.youtube.com/watch?v=P_6W-36QfLA
+//IsGrounded code found at: https://www.youtube.com/watch?v=P_6W-36QfLA
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+
+    [Header("Player Speed")]
     [SerializeField] private float walkSpeed;
     [SerializeField] private float jumpSpeed;
     [SerializeField] private float crouchSpeed;
@@ -18,21 +20,26 @@ public class PlayerMovement : MonoBehaviour
     private KeyCode jumpKey = KeyCode.Space;
     private KeyCode crouchKey = KeyCode.S;
 
+    [Header("Player Size")]
     private float startScaleY = 1f;
     private float crouchScaleY = 0.5f;
+
+    [Header("Check Ground")]
+    [SerializeField] private Vector2 boxSize;
+    [SerializeField] private LayerMask groundLayer;
+    private float castDistance = 0.95f;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.freezeRotation = true; //Keeps player upright
+        rb.freezeRotation = true; //Keep player upright
         moveSpeed = walkSpeed;
     }
 
     private void Update()
     {
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y); //Left and right movement
-
-        if (Input.GetKeyDown(jumpKey)) //Jump
+        if (Input.GetKeyDown(jumpKey) && IsGrounded()) //Jump
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
         }
@@ -49,5 +56,21 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector2(transform.localScale.x, startScaleY);
             moveSpeed = walkSpeed;
         }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y); //Left and right movement
+    }
+
+    public bool IsGrounded()
+    {
+        if(Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer)) { return true; }
+        else { return false; }
+    }
+
+    private void OnDrawGizmos() //Testing
+    {
+        Gizmos.DrawWireCube(transform.position - transform.up * castDistance, boxSize); //Display BoxCast
     }
 }
